5e897a68f5c76b9ed02989ebb5e5ebc01f41e23e
Larath <fec25@yahoo.com>
2022-06-21 16:22:00 -0400

    state, setState, onClick event update page value

--- react/monsters-rolodex/src/App.js
+++ react/monsters-rolodex/src/App.js
@@ -1,18 +1,28 @@
-import { component } from 'react'
+import { Component } from 'react'
 
 import logo from './logo.svg';
 import './App.css';
 
-class App extends component {
+class App extends Component {
+  constructor(){
+    super(); //calls the constructor of the underlying component
 
-  render () {
+    this.state = {
+      name:{firstName:'John',lastName:'Doe'},
+      company:'McDonalds'
+    }
+  }
+
+  render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p>Hi, {this.state.name.firstName} {this.state.name.lastName} from {this.state.company}</p>
+          <button onClick={() => {
+            this.setState({name:{firstName:'Jimmie', lastName:'2Shoes'}});
+            console.log(this.state)
+          }}>Click to Change</button>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -25,7 +35,6 @@ class App extends component {
       </div>
     );
   }
-  
 }
 
 export default App;
