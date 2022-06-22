ca03c840003a01b19edb09a851154c59f4ef1808
Larath <fec25@yahoo.com>
2022-06-22 09:28:02 -0400

    arrays, map

--- react/monsters-rolodex/src/App.js
+++ react/monsters-rolodex/src/App.js
@@ -8,30 +8,37 @@ class App extends Component {
     super(); //calls the constructor of the underlying component
 
     this.state = {
-      name:{firstName:'John',lastName:'Doe'},
-      company:'McDonalds'
-    }
+      monsters:[], //array
+    };
   }
 
+ //componentDidMount runs only once when the component is mounted
+  componentDidMount(){
+    //fetch is a promise
+     fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response)=> response.json()) //convert response to json
+       .then((users) => { //from the resposne create user
+        this.setState(
+          () => {
+            return {monsters: users}; //sets array to eq. user
+          },
+          () =>{
+            console.log(this.state);
+          }
+        )
+      }) 
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi, {this.state.name.firstName} {this.state.name.lastName} from {this.state.company}</p>
-          <button onClick={() => {
-            this.setState({name:{firstName:'Jimmie', lastName:'2Shoes'}});
-            console.log(this.state)
-          }}>Click to Change</button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.monsters.map((monster)=>{ //maps it erated monsters object to monster object
+          return ( //key is used by React to identify what component needs to be re-rendered
+            <div key={monster.id}>
+            <h1>{monster.name}</h1> 
+            </div>
+          )
+        })}
       </div>
     );
   }
