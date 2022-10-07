7a855be55d6c23da5d116fe68d5798fa30f67c91
Eric Calkins <fcalkins@paychex.com>
2022-10-07 15:04:12 -0400

    created authentication page component & signin frm

--- react/crwn-shopping/src/App.js
+++ react/crwn-shopping/src/App.js
@@ -2,7 +2,8 @@ import { Routes, Route } from 'react-router-dom'
 
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
-import SignIn from './routes/sign-in/sign-in';
+import Authentication from './routes/authentication/authentication.component';
+
 
 const Shop = () => {
   return <h1>This is the shop</h1>
@@ -14,7 +15,7 @@ const App = () => {
       <Route path='/' element={ <Navigation /> }>
         <Route index element= { <Home /> } />
         <Route path='/shop' element = { <Shop /> } />
-        <Route path='/sign-in' element = { <SignIn /> } />
+        <Route path='/auth' element = { <Authentication /> } />
       </Route>
     </Routes>
   )
--- react/crwn-shopping/src/utils/firebase/firebase.utils.js
+++ react/crwn-shopping/src/utils/firebase/firebase.utils.js
@@ -3,7 +3,9 @@ import {
     getAuth, 
     signInWithRedirect, 
     signInWithPopup, 
-    GoogleAuthProvider 
+    GoogleAuthProvider,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword
 } from 'firebase/auth';
 
 import {
@@ -28,24 +30,24 @@ const firebaseConfig = {
   // Initialize Firebase
   const firebaseApp = initializeApp(firebaseConfig);
 
-  const provider = new GoogleAuthProvider();
-  provider.setCustomParameters({
+  const googleProvider = new GoogleAuthProvider();
+  googleProvider.setCustomParameters({
     prompt: "select_account"
   });
 
   export const auth = getAuth();
-  export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+  export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
+  export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
   
   export const db = getFirestore();
 
-  export const createUserDocumentFromAuth = async(userAuth) => {
+  export const createUserDocumentFromAuth = async(userAuth, additionalInformation = {}) => {
+    if(!userAuth) return;
+
     const userDocRef = doc(db, 'users', userAuth.uid);
 
-    console.log(userDocRef)
-
     const userSnapshot = await getDoc(userDocRef);
-    console.log(userSnapshot.exists());
-
+    
     if(!userSnapshot.exists()) {
         const {displayName, email } = userAuth
         const createdAt = new Date();
@@ -54,7 +56,8 @@ const firebaseConfig = {
             await setDoc(userDocRef, {
                 displayName,
                 email,
-                createdAt
+                createdAt,
+                ...additionalInformation,
             })
         }
         catch(error){
@@ -64,4 +67,14 @@ const firebaseConfig = {
 
         return userDocRef;
     }
+  }
+
+  export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+    if(!email||!password) return; 
+    return await signInWithEmailAndPassword(auth, email, password)
+  }
+
+  export const createAuthUserWithEmailAndPassword = async (email, password) => {
+    if(!email||!password) return; 
+    return await createUserWithEmailAndPassword(auth, email, password)
   }
